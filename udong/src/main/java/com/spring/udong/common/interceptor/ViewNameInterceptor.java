83d63d48096aa841472ddfbe53736b1192a2fbd9
JiHyeonBeak <jadehl09@gmail.com>
2021-09-16 21:54:02 +0900

    fixing

---
+++ udong/src/main/java/com/spring/udong/common/interceptor/ViewNameInterceptor.java
@@ -0,0 +1,7 @@
+package com.spring.udong.common.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class ViewNameInterceptor extends HandlerInterceptorAdapter {
+
+}
--- udong/src/main/java/com/spring/udong/join/service/MemberService.java
+++ udong/src/main/java/com/spring/udong/join/service/MemberService.java
@@ -1,5 +1,14 @@
 package com.spring.udong.join.service;
 
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+
+import com.spring.udong.join.vo.MemberVO;
+
 public interface MemberService {
+	public List listMembers() throws DataAccessException;
+	public int addMember(MemberVO memberVO) throws DataAccessException;
+	public int removeMember(String id) throws DataAccessException;
 
 }
