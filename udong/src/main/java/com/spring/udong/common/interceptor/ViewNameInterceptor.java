fcbb95875ea5a26e039f425d3211acf8a52f0c56
PigSheep <84897199+JiHyeonBeak@users.noreply.github.com>
2021-09-18 22:20:55 +0900

    Delete udong directory
    wrong file

--- udong/src/main/java/com/spring/udong/common/interceptor/ViewNameInterceptor.java
+++
@@ -1,7 +0,0 @@
-package com.spring.udong.common.interceptor;
-
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
-public class ViewNameInterceptor extends HandlerInterceptorAdapter {
-
-}
